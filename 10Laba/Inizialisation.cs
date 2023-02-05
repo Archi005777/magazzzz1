ee20e0a0bd852896b42604cf10f22690796c115a
Archi005777 <115105146+Archi005777@users.noreply.github.com>
2023-02-05 22:16:17 +0300

    Add files via upload

---
+++ 10Laba/Inizialisation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10Laba
+{
+    internal class Inizialisation
+    {
+        public static List<Data> PersonalList = new List<Data>() { new Data("Administrator", "111", "111", 0) };
+        public static string ChoiseRole;
+        public static bool log;
+        public static string file = "10praka.json";
+        public static List<Data> newPersonalList = Serializator.myDeser<List<Data>>(file);
+        public static bool init()
+        {
+            foreach (var item in newPersonalList)
+            {
+
+                if (Menu.pass == item.Password && item.Role == Personal.Administrator.ToString() && Menu.login == item.Login)
+                {
+                    ChoiseRole = Personal.Administrator.ToString();
+                    log = true;
+                }
+                else if (Menu.pass == item.Password && item.Role == Personal.Kadrovik.ToString()  && Menu.login == item.Login)
+                {
+                    ChoiseRole = Personal.Kadrovik.ToString();
+                    log = true;
+                }
+            }
+            if(log == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
